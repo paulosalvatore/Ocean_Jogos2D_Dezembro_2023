257003ca6ceb6375a65bfa0efa22dcb43c892fd6
Paulo Salvatore <salvatoregames@gmail.com>
2023-12-07 11:42:27 -0300

    Destruindo o Bullet após um tempo

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -6,10 +6,12 @@ public class BulletController : MonoBehaviour
 {
     public float speed = 10f;
 
+    public float delayToDestroy = 15f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, delayToDestroy);
     }
 
     // Update is called once per frame
