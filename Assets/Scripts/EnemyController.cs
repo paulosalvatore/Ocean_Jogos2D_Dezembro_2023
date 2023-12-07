a1e816c75a66439daf215f33df398107c348f9ef
Paulo Salvatore <salvatoregames@gmail.com>
2023-12-07 10:00:50 -0300

    Movimentação do Inimigo

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    Rigidbody2D rb;
+
+    public float speed = 2f;
+
+    GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var direction = player.transform.position - transform.position;
+
+        rb.velocity = direction.normalized * speed;
     }
 }
